a4abb6e71a87e0d963b55304cba5a8db792ebe86
Rom4nVK <110935146+Rom4nVK@users.noreply.github.com>
2023-06-19 19:24:28 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+// ЗАДАЧА 10
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int num1 = number % 100 / 10;
+// if (number > 100 & number < 1000 )
+// {Console.WriteLine("цифра " + num1 );}
+// else
+// {Console.WriteLine("ошибка "  );}
+
+// ЗАДАЧА 13
+
+Console.WriteLine("Введите число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int num1 = number % 10;
+int num2 = number % 100 / 10;
+ if (number > 100 &  number < 1000)
+{Console.WriteLine("цифра " + num1 );}
+
+else if (number < 100)
+{Console.WriteLine("третьей цифры нет "  );}
+
+else
+{Console.WriteLine( + num2  );}
+
+
+// // // ЗАДАЧА 15
+
+// Console.WriteLine("Введите число");
+
+// int number = Convert.ToInt32(Console.ReadLine());
+
+//  if (number > 5 &  number < 8)
+// {Console.WriteLine("выходной " );}
+
+// else if (number <= 5 & number >= 5 )
+// {Console.WriteLine("Рабочий день "  );}
+
+// else
+// {Console.WriteLine( "ERRRRRRRROR"  );}
+
+
+
